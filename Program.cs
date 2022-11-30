318c399b11a568f00a17dc3d19290888dc621070
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-11-30 14:26:04 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadAndWriteFilesChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] map = File.ReadAllLines(@"Mapfile.txt");
+                        
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+                Console.WriteLine(map[y]);
+            }
+
+            Console.ReadKey();
+
+            File.WriteAllLines("Savefile.txt", map);
+
+            Console.ReadKey();
+        }
+    }    
+}
